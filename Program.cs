d0fbd419191e52cb9e6e10fa71b95d736e2a1b89
Сергей Пилькевич <sergpilk33@gmail.com>
2023-09-14 16:41:50 +0300

    Calculate

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/*Console.WriteLine("Хотите ли вы сахар в чае: ");
+string txt = Console.ReadLine();
+while (txt == "да")
+{
+    Console.WriteLine("Добавляем сахар");
+    Console.WriteLine("Еще?");
+    txt = Console.ReadLine();
+}*/
+
+/*
+Console.WriteLine("Сколько ложек сахара вы хотите: ");
+int txt = Convert.ToInt32(Console.ReadLine());
+int cl = 1;
+while (cl!=txt+1)
+{
+    Console.WriteLine("Добавил " + cl + " ложек сахара");
+    cl ++;
+}*/
+
+int k = 1000;
+while (k>0)
+    {
+    Console.WriteLine(Convert.ToString(k) + " - 7 = " + Convert.ToString(k-7));
+    k -= 7;
+}
